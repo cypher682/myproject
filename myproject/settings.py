3e90bdef6cc1c15c086ba1109ca8d2b23487f4c3
Ubuntu <ubuntu@ip-172-31-84-116.ec2.internal>
2025-08-03 17:32:32 +0000

    reinstall gunicorn again

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kk9kd45bbvu2x6g0-%l_5^oe@@h8=)ebos3%i*_j&lkac1rjn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.90.4.60']
 
 
 # Application definition
